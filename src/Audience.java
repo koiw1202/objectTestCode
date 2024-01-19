cb752a81ba7d30f5c22fa97a3a919467b68ce1e8
koiw1202 <koiw1202@naver.com>
2024-01-19 19:38:44 +0900

    First Commit

--- src/Audience.java
+++ src/Audience.java
@@ -9,11 +9,15 @@ public class Audience {
     private Bag bag ;
 
     public Audience(Bag bag) {
+
         this.bag = bag ;
+
     }
 
     public Bag getBag() {
+
         return bag ;
+
     }
 
 
